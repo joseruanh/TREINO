cfab09885c180f0ba7fdee544b009882cceb5ea7
Ruanh <79330021+joseruanh@users.noreply.github.com>
2026-04-28 18:51:14 -0300

    adicionando conteudo e formatacao

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function segunda()
+{
+    let a = window.document.getElementById("res")
+    a.innerHTML = ` 
+    <p class="treinoT">Treino de superiores</p>
+    <hr>
+    <p > Abdominais, braços, costas...
+    <br>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/eOztWENT9ao?si=vluraZCxRPNxGP-L" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    <p>Treino para Queimar Gordura e Tonificar Abdômen e Braços em casa</p> <hr>
+    <br><br><br>
+    <p class="treinoT"> Cardio </p>
+    <hr>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/kSolRWo1d38?si=Oyj7GRIkccn3GERL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe><hr>
+    <br><br><br>
+     `
+}
